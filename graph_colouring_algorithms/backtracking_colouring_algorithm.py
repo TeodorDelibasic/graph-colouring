982b76a5533357696c29364e527be5e9eddd5e26
Teodor Delibasic <teodordelibasic@gmail.com>
2023-09-19 09:06:38 +0200

    Add Backtracking

---
+++ graph_colouring_algorithms/backtracking_colouring_algorithm.py
@@ -0,0 +1,34 @@
+from graph_colouring_algorithms.abstract_colouring_algorithm import AbstractColouringAlgorithm
+
+
+class BacktrackingColouringAlgorithm(AbstractColouringAlgorithm):
+
+    def is_safe(self, graph, vertex, colour):
+        for neighbour in graph.vertices[vertex][1]:
+            if colour == graph.vertices[neighbour][0]:
+                return False
+        return True
+
+    def colour_graph(self, graph):
+        self.colour_graph_util(graph, 0, len(graph.vertices))
+
+    def colour_graph_util(self, graph, index, n):
+        if index == len(graph.vertices):
+            return True
+
+        vertex = list(graph.vertices.keys())[index]
+        properties = graph.vertices[vertex]
+
+        for colour in range(n):
+            if self.is_safe(graph, vertex, colour):
+                properties[0] = colour
+
+                if self.colour_graph_util(graph, index + 1, n):
+                    return True
+
+                properties[0] = -1
+
+        return False
+
+    def __str__(self):
+        return "Backtracking Colouring Algorithm"
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from random_graph_generator import RandomGraphGenerator
 from graph import Graph
 from graph_colouring_algorithms.greedy_colouring_algorithm import GreedyColouringAlgorithm
 from graph_colouring_algorithms.welsh_powell_colouring_algorithm import WelshPowellColouringAlgorithm
+from graph_colouring_algorithms.backtracking_colouring_algorithm import BacktrackingColouringAlgorithm
 from my_error import MyError
 
 
 def main():
     try:
-        graph: Graph = RandomGraphGenerator.generate_for_edges(25)
+        graph: Graph = RandomGraphGenerator.generate_for_edges(100)
 
         print(graph)
 
@@ -15,6 +16,9 @@ def main():
         graph.colour()
 
         graph.set_graph_colouring_algorithm(WelshPowellColouringAlgorithm())
+        graph.colour()
+
+        graph.set_graph_colouring_algorithm(BacktrackingColouringAlgorithm())
         graph.colour()
     except MyError as e:
         print(e)
